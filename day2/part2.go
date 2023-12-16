579ba7aa4155988f28cea7d5fbc9be3e3f9b3ca1
Sanket Dawar <sanket.dawar@indiamart.com>
2023-12-17 01:22:57 +0530

    day2 part2

--- day2/part2.go
+++ day2/part2.go
@@ -21,15 +21,15 @@ type Game struct {
 }
 
 var (
-	GameIdCount *int
+	SumOfPower *int
 )
 
 func main() {
-	GameIdCount = new(int)
+	SumOfPower = new(int)
 	start := time.Now()
 	readFile()
 
-	fmt.Println("GameIdCount: ", *GameIdCount)
+	fmt.Println("SumOfPower: ", *SumOfPower)
 	fmt.Println("time: ", time.Since(start))
 }
 
@@ -40,15 +40,11 @@ func readFile() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	i := 5
+
 	for scanner.Scan() {
 		text := scanner.Text()
-		if i>0{
-			getGameData(text)
-			fmt.Println("scanner.Text(): ", text)
-			i--
-		}
-
+		getGameData(text)
+		// fmt.Println("scanner.Text(): ", text)
 	}
 	if err != nil {
 		log.Fatal(err)
@@ -62,30 +58,35 @@ func getGameData(line string) {
 	strlen := len(line) - 1
 	blue := "blue"
 	green := "green"
+	fmt.Println("red: ", game.red)
 	for index, val := range line {
 		if val == ':' {
 			game.Id, _ = strconv.Atoi(line[idPos:index])
 			fmt.Println(line[idPos:index])
 		}
 		if index < strlen-1 && strings.Contains(line[index:index+3], red) {
-			temp, _ := strconv.Atoi(line[index-3 : index-1])
+			temp, _ := strconv.Atoi(strings.TrimSpace(line[index-3 : index-1]))
 				if game.red <temp{
 					game.red = temp
+					// fmt.Printf("\nnew red: %v", game.red)
 				}
 		}
 		if index < strlen-2 && strings.Contains(line[index:index+4], blue) {
-			temp, _ := strconv.Atoi(line[index-3 : index-1])
+			temp, _ := strconv.Atoi(strings.TrimSpace(line[index-3 : index-1]))
 				if game.blue <temp{
 					game.blue = temp
+					// fmt.Printf("\nnew blue: %v", game.blue)
 				}
 		}
 		if index < strlen-3 && strings.Contains(line[index:index+5], green) {
-			temp, _ := strconv.Atoi(line[index-3 : index-1])
+			temp, _ := strconv.Atoi(strings.TrimSpace(line[index-3 : index-1]))
 				if game.green <temp{
 					game.green = temp
+					// fmt.Printf("\nnew green: %v", game.green)
 				}
 		}
 
 	}
 	fmt.Println("game: ", game)
+	*SumOfPower += game.red * game.blue * game.green
 }
